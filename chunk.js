69c377efabb371639da935238848c17b9a707817
David Carne <david@carnelec.com>
2010-09-21 18:03:06 -0700

    Fixed lighting index calculation, generalized chunk.js

--- chunk.js
+++ chunk.js
@@ -4,6 +4,9 @@ Chunk = function () {
 	this.sizeX = 16;
 	this.sizeY = 128;
 	this.sizeZ = 16;
+
+	this.sectionSize = this.sizeX * this.sizeY * this.sizeZ;
+
 	this.data = new Buffer(this.sizeX * this.sizeY * this.sizeZ * 2.5);
 	for (var i = 0; i < this.data.length; i++) {
 		this.data[i] = 0;
@@ -18,19 +21,19 @@ Chunk.prototype.setType = function (x, y, z, type) {
 	this.data[this.indexOf(x,y,z)] = type;
 }
 Chunk.prototype.setMetadata = function (x, y, z, meta) {
-	this.data[this.indexOf(x,y,z) + 16*128*16] = meta;
+	this.data[this.indexOf(x,y,z) + this.sectionSize] = meta;
 }
 Chunk.prototype.setLighting = function (x, y, z, lighting) {
 	var idx = this.indexOf(x,y,z)/2;
 	var byte = Math.floor(idx);
-	var top = idx % 2 == 0;
-	var value = this.data[byte + 16*128*16 + 16*128*16];
+	var top = byte % 2 == 0;
+	var value = this.data[byte + this.sectionSize * 2];
 	if (top) {
 		value = (value & 0xf) | ((lighting & 0xf) << 4);
 	} else {
 		value = (value & 0xf0) | (lighting & 0xf);
 	}
-	this.data[byte + 16*128*16 + 16*128*16] = value;
+	this.data[byte + this.sectionSize * 2] = 0xFF;
 }
 
 exports.Chunk = Chunk;
